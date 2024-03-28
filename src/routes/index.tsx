a2a5a8a4557a16109897b5a2d3fd5c45253fede9
nadavzeira <nadavzeira@gmail.com>
2024-03-28 15:19:32 +0200

    Rename and temp comment

---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+
+import UserList from "../components/UserList";
+// import UserModal from '../components/UserModal'
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <UserList />
+      <Switch>{/* <Route path="/:id" component={UserModal}/> */}</Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
